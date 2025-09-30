e6368b7e4105e3f38657f3b756ecc30a962a2dff
VisualControl <system.visualcontrol@icloud.com>
2025-09-30 09:23:30 +0200

    added class AuthorizedUser

---
+++ Sources/VisualControlSharedDTO/0 DataModels/AuthorizedUser.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizedUser.swift
+//  VisualControlSharedDTO
+//
+//  Created by Systemintegration VisualControl on 30.09.25.
+//
+
+import Foundation
+
+final class AuthorizedUser {
+    
+    let authUsereName: String
+    
+    init(authUsereName: String) {
+        self.authUsereName = authUsereName
+    }
+}
