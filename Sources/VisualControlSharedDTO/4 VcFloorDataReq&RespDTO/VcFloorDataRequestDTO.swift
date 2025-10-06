ad2dd77b428082972519c707e6c9afcc93c81fd9
VisualControl <system.visualcontrol@icloud.com>
2025-10-06 14:25:33 +0200

    update floor data req&resp DTO

--- Sources/VisualControlSharedDTO/4 VcFloorDataReq&RespDTO/VcFloorDataRequestDTO.swift
+++ Sources/VisualControlSharedDTO/4 VcFloorDataReq&RespDTO/VcFloorDataRequestDTO.swift
@@ -9,12 +9,12 @@ import Foundation
 
 public struct VcFloorDataRequestDTO: Codable, @unchecked Sendable {
     
-    public var sequenceNumber: Int
-    public var floorNameVisu: String
-    public var floorNameBaos: String
-    public var creatingDate: String
-    public var cleaningButtonsType: Int // Cleaning buttons for each floor (0-none, 1-Blinds, 2-Awnings)
-    public var knxMainGroup: Int
+    public let sequenceNumber: Int
+    public let floorNameVisu: String
+    public let floorNameBaos: String
+    public let creatingDate: String
+    public let cleaningButtonsType: Int // Cleaning buttons for each floor (0-none, 1-Blinds, 2-Awnings)
+    public let knxMainGroup: Int
     
     init(sequenceNumber: Int,
          floorNameVisu: String,
--- Sources/VisualControlSharedDTO/4 VcFloorDataReq&RespDTO/VcFloorDataResponseDTO.swift
+++ Sources/VisualControlSharedDTO/4 VcFloorDataReq&RespDTO/VcFloorDataResponseDTO.swift
@@ -9,13 +9,13 @@ import Foundation
 
 public struct VcFloorDataResponseDTO: Codable, @unchecked Sendable {
     
-    public var id: UUID
-    public var sequenceNumber: Int
-    public var floorNameVisu: String
-    public var floorNameBaos: String
-    public var creatingDate: String
-    public var cleaningButtonsType: Int // Cleaning buttons for each floor (0-none, 1-Blinds, 2-Awnings)
-    public var knxMainGroup: Int
+    public let id: UUID
+    public let sequenceNumber: Int
+    public let floorNameVisu: String
+    public let floorNameBaos: String
+    public let creatingDate: String
+    public let cleaningButtonsType: Int // Cleaning buttons for each floor (0-none, 1-Blinds, 2-Awnings)
+    public let knxMainGroup: Int
     
     init(id: UUID,
          sequenceNumber: Int,
