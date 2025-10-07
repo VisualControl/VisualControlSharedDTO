455df62073ad72ad6fe7248b9a6e8f5e3d3f2895
VisualControl <system.visualcontrol@icloud.com>
2025-10-07 16:16:29 +0200

    update files

--- Sources/VisualControlSharedDTO/4 VcFloorDataReq&RespDTO/VcFloorDataRequestDTO.swift
+++ Sources/VisualControlSharedDTO/4 VcFloorDataReq&RespDTO/VcFloorDataRequestDTO.swift
@@ -9,21 +9,18 @@ import Foundation
 
 public struct VcFloorDataRequestDTO: Codable, @unchecked Sendable {
     
-    public let sequenceNumber: Int
     public let floorNameVisu: String
     public let floorNameBaos: String
     public let cleaningButtonsType: Int // Cleaning buttons for each floor (0-none, 1-Blinds, 2-Awnings)
     public let knxMainGroup: Int
     public let authorizedUsers: [String]
     
-    public init(sequenceNumber: Int,
-                floorNameVisu: String,
+    public init(floorNameVisu: String,
                 floorNameBaos: String,
                 cleaningButtonsType: Int,
                 knxMainGroup: Int,
                 authorizedUsers: [String])
     {
-        self.sequenceNumber = sequenceNumber
         self.floorNameVisu = floorNameVisu
         self.floorNameBaos = floorNameBaos
         self.cleaningButtonsType = cleaningButtonsType
