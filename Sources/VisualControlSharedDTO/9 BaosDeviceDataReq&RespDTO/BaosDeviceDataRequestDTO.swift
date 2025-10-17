9cd087cbdb33a0ce3bf1264fd8e757749e653ae0
VisualControl <system.visualcontrol@icloud.com>
2025-10-17 10:46:48 +0200

    Update BaosDeviceDataRequestDTO.swift

--- Sources/VisualControlSharedDTO/9 BaosDeviceDataReq&RespDTO/BaosDeviceDataRequestDTO.swift
+++ Sources/VisualControlSharedDTO/9 BaosDeviceDataReq&RespDTO/BaosDeviceDataRequestDTO.swift
@@ -12,19 +12,16 @@ public struct BaosDeviceDataRequestDTO: Codable, @unchecked Sendable {
     public let sequenceNumber: Int
     public let ipAddress: String
     public let deviceNameVc: String
-    public let creatingDate: Date
     public let token: String
     
     public init(sequenceNumber: Int,
                 ipAddress: String,
                 deviceNameVc: String,
-                creatingDate: Date,
                 token: String)
     {
         self.sequenceNumber = sequenceNumber
         self.ipAddress = ipAddress
         self.deviceNameVc = deviceNameVc
-        self.creatingDate = creatingDate
         self.token = token
     }
     
