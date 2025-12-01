b8414611c44c08d6b36ff45dbd4fe99a0d2d49df
VisualControl <system.visualcontrol@icloud.com>
2025-12-01 20:28:13 +0100

    update BaosWebSocketResponseDTO

--- Sources/VisualControlSharedDTO/9 BaosDeviceDataReq&RespDTO/BaosWebSocketResponseDTO.swift
+++ Sources/VisualControlSharedDTO/9 BaosDeviceDataReq&RespDTO/BaosWebSocketResponseDTO.swift
@@ -10,15 +10,13 @@ import Foundation
 public struct BaosWebSocketResponseDTO: Codable, @unchecked Sendable {
     
     public let id: Int
-    public let type: String
     public let format: String
     public let rawValue: [Int]
     public let isUpdated: Bool
     public let isValid: Bool
     
-    public init(id: Int, type: String, format: String, rawValue: [Int], isUpdated: Bool, isValid: Bool) {
+    public init(id: Int, format: String, rawValue: [Int], isUpdated: Bool, isValid: Bool) {
         self.id = id
-        self.type = type
         self.format = format
         self.rawValue = rawValue
         self.isUpdated = isUpdated
