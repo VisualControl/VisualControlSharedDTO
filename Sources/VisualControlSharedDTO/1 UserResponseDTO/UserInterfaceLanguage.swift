f7c5e9b60095844c3b2991149d93e0078a41833b
VisualControl <system.visualcontrol@icloud.com>
2026-01-05 20:38:15 +0100

    Added UserInterfaceLanguage

---
+++ Sources/VisualControlSharedDTO/1 UserResponseDTO/UserInterfaceLanguage.swift
@@ -0,0 +1,15 @@
+//
+//  UserInterfaceLanguage.swift
+//  VisualControlSharedDTO
+//
+//  Created by Systemintegration VisualControl on 05.01.26.
+//
+
+import Foundation
+
+public enum UserInterfaceLanguage: String, Codable, @unchecked Sendable {
+    
+    case English
+    case German
+    
+}
