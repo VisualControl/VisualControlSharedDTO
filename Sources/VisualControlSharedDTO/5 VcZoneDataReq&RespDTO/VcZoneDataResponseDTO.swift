52dde04bf177d78c0d4d1b574c8fac5b7afbab68
VisualControl <system.visualcontrol@icloud.com>
2026-01-05 13:36:57 +0100

    Update Product IDs

--- Sources/VisualControlSharedDTO/5 VcZoneDataReq&RespDTO/VcZoneDataResponseDTO.swift
+++ Sources/VisualControlSharedDTO/5 VcZoneDataReq&RespDTO/VcZoneDataResponseDTO.swift
@@ -16,6 +16,12 @@ public struct VcZoneDataResponseDTO: Codable, @unchecked Sendable {
     public let creatingDate: Date
     public let productType: ProductType
     public let authorizedUsers: [String]
+    public var controllerStateListId: UUID?
+    public var productBlindId: UUID?
+    public var productAwningId: UUID?
+    public var productLightId: UUID?
+    public var productWellumicDirectId: UUID?
+    public var productWellumicDirIndirId: UUID?
     
     public init(id: UUID,
                 sequenceNumber: Int,
@@ -23,7 +29,13 @@ public struct VcZoneDataResponseDTO: Codable, @unchecked Sendable {
                 zoneNameBaos: String,
                 creatingDate: Date,
                 productType: ProductType,
-                authorizedUsers: [String])
+                authorizedUsers: [String],
+                controllerStateListId: UUID? = nil,
+                productBlindId: UUID? = nil,
+                productAwningId: UUID? = nil,
+                productLightId: UUID? = nil,
+                productWellumicDirectId: UUID? = nil,
+                productWellumicDirIndirId: UUID? = nil)
     {
         self.id = id
         self.sequenceNumber = sequenceNumber
@@ -32,5 +44,11 @@ public struct VcZoneDataResponseDTO: Codable, @unchecked Sendable {
         self.creatingDate = creatingDate
         self.productType = productType
         self.authorizedUsers = authorizedUsers
+        self.controllerStateListId = controllerStateListId
+        self.productBlindId = productBlindId
+        self.productAwningId = productAwningId
+        self.productLightId = productLightId
+        self.productWellumicDirectId = productWellumicDirectId
+        self.productWellumicDirIndirId = productWellumicDirIndirId
     }
 }
