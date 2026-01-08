216a4372201653289759a7e91e332f99359f2c81
VisualControl <system.visualcontrol@icloud.com>
2026-01-08 22:43:17 +0100

    Update VcHeaderValuesResponseDTO.swift

--- Sources/VisualControlSharedDTO/3 VcHeaderDataReq&RespDTO/VcHeaderValuesResponseDTO.swift
+++ Sources/VisualControlSharedDTO/3 VcHeaderDataReq&RespDTO/VcHeaderValuesResponseDTO.swift
@@ -9,10 +9,10 @@ import Foundation
 
 public struct VcHeaderValuesResponseDTO: Codable, @unchecked Sendable {
     
-    public let currentWindSpeed1To4: [Float]
-    public let currentWindDirection1To4: [Int]
-    public let currentBrightness1To8: [Int]
-    public let currentSolarEnergy1To8: [Int]
+    public let currentWindSpeed1to4: [Float]
+    public let currentWindDirection1to4: [Int]
+    public let currentBrightness1to8: [Int]
+    public let currentSolarEnergy1to8: [Int]
     public let currentOutTemp: Float
     public let currentRainState: Bool
     public let currentIceState: Bool
@@ -20,10 +20,10 @@ public struct VcHeaderValuesResponseDTO: Codable, @unchecked Sendable {
     public let currentTime: Date
     public let currentDate: Date
     
-    public init(currentWindSpeed1To4: [Float],
-                currentWindDirection1To4: [Int],
-                currentBrightness1To8: [Int],
-                currentSolarEnergy1To8: [Int],
+    public init(currentWindSpeed1to4: [Float],
+                currentWindDirection1to4: [Int],
+                currentBrightness1to8: [Int],
+                currentSolarEnergy1to8: [Int],
                 currentOutTemp: Float,
                 currentRainState: Bool,
                 currentIceState: Bool,
@@ -31,10 +31,10 @@ public struct VcHeaderValuesResponseDTO: Codable, @unchecked Sendable {
                 currentTime: Date,
                 currentDate: Date)
     {
-        self.currentWindSpeed1To4 = currentWindSpeed1To4
-        self.currentWindDirection1To4 = currentWindDirection1To4
-        self.currentBrightness1To8 = currentBrightness1To8
-        self.currentSolarEnergy1To8 = currentSolarEnergy1To8
+        self.currentWindSpeed1to4 = currentWindSpeed1to4
+        self.currentWindDirection1to4 = currentWindDirection1to4
+        self.currentBrightness1to8 = currentBrightness1to8
+        self.currentSolarEnergy1to8 = currentSolarEnergy1to8
         self.currentOutTemp = currentOutTemp
         self.currentRainState = currentRainState
         self.currentIceState = currentIceState
