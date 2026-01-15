15cf1f006108c21f9b30de26ce22f6d2b1c2ba4c
VisualControl <system.visualcontrol@icloud.com>
2026-01-15 10:15:11 +0100

    Added zone product response DTO

---
+++ Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesAwningResponseDTO.swift
@@ -0,0 +1,21 @@
+//
+//  BaosDeviceZoneValuesAwningResponseDTO.swift
+//  VisualControlSharedDTO
+//
+//  Created by Systemintegration VisualControl on 15.01.26.
+//
+
+import Foundation
+
+public struct BaosDeviceZoneValuesAwningResponseDTO: Codable, @unchecked Sendable {
+    
+    public let controllerState: Int
+    public let controllerStateDescription: String
+    public let currentAwningPosition: Int
+    
+    init(controllerState: Int, controllerStateDescription: String, currentAwningPosition: Int) {
+        self.controllerState = controllerState
+        self.controllerStateDescription = controllerStateDescription
+        self.currentAwningPosition = currentAwningPosition
+    }
+}
---
+++ Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesBlindResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  BaosDeviceZoneValuesBlindResponseDTO.swift
+//  VisualControlSharedDTO
+//
+//  Created by Systemintegration VisualControl on 15.01.26.
+//
+
+import Foundation
+
+public struct BaosDeviceZoneValuesBlindResponseDTO: Codable, @unchecked Sendable {
+    
+    public let controllerState: Int
+    public let controllerStateDescription: String
+    public let currentBlindPosition: Int
+    public let currentSlatPosition: Int
+    
+    init(controllerState: Int, controllerStateDescription: String, currentBlindPosition: Int, currentSlatPosition: Int) {
+        self.controllerState = controllerState
+        self.controllerStateDescription = controllerStateDescription
+        self.currentBlindPosition = currentBlindPosition
+        self.currentSlatPosition = currentSlatPosition
+    }    
+}
