f06d454a537393a0f536cd2b17f676a2ba30e1b9
VisualControl <system.visualcontrol@icloud.com>
2026-01-15 20:34:52 +0100

    Update BaosDeviceZoneValuesBlindResponseDTO.swift

--- Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesBlindResponseDTO.swift
+++ Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesBlindResponseDTO.swift
@@ -12,12 +12,12 @@ public struct BaosDeviceZoneValuesBlindResponseDTO: Codable, @unchecked Sendable
     public let controllerState: Int
     public var controllerStateDescription: String
     public let currentBlindPosition: Int
-    public let currentSlatPosition: Int
+    public let currentSlatAngle: Int
     
-    public init(controllerState: Int, controllerStateDescription: String, currentBlindPosition: Int, currentSlatPosition: Int) {
+    public init(controllerState: Int, controllerStateDescription: String, currentBlindPosition: Int, currentSlatAngle: Int) {
         self.controllerState = controllerState
         self.controllerStateDescription = controllerStateDescription
         self.currentBlindPosition = currentBlindPosition
-        self.currentSlatPosition = currentSlatPosition
-    }    
+        self.currentSlatAngle = currentSlatAngle
+    }
 }
