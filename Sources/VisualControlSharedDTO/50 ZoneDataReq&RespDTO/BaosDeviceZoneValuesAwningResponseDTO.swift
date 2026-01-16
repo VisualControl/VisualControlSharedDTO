04817230f9e4d891e21bbb198fb19b76c66a258c
VisualControl <system.visualcontrol@icloud.com>
2026-01-16 10:20:22 +0100

    Update BaosDeviceZoneValuesAwningResponseDTO.swift

--- Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesAwningResponseDTO.swift
+++ Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesAwningResponseDTO.swift
@@ -9,13 +9,26 @@ import Foundation
 
 public struct BaosDeviceZoneValuesAwningResponseDTO: Codable, @unchecked Sendable {
     
-    public let controllerState: Int
+    public let zoneNameBaos: String
+    public let currentControllerState: Int
     public var controllerStateDescription: String
     public let currentAwningPosition: Int
+    public let controllerStateId: UUID?
+    public let awningPositionId: UUID?
     
-    public init(controllerState: Int, controllerStateDescription: String, currentAwningPosition: Int) {
-        self.controllerState = controllerState
+    public init(zoneNameBaos: String,
+                currentControllerState: Int,
+                controllerStateDescription: String,
+                currentAwningPosition: Int,
+                controllerStateId: UUID?,
+                awningPositionId: UUID?)
+    {
+        self.zoneNameBaos = zoneNameBaos
+        self.currentControllerState = currentControllerState
         self.controllerStateDescription = controllerStateDescription
         self.currentAwningPosition = currentAwningPosition
+        self.controllerStateId = controllerStateId
+        self.awningPositionId = awningPositionId
     }
+    
 }
