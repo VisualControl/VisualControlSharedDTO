453b17125f7ff8bb44482a341cecad569ff01245
VisualControl <system.visualcontrol@icloud.com>
2026-01-16 16:32:05 +0100

    Update BaosDeviceZoneValuesBlindsAndAwningsRequestDTO.swift

--- Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesBlindsAndAwningsRequestDTO.swift
+++ Sources/VisualControlSharedDTO/50 ZoneDataReq&RespDTO/BaosDeviceZoneValuesBlindsAndAwningsRequestDTO.swift
@@ -10,10 +10,18 @@ import Foundation
 public struct BaosDeviceZoneValuesBlindsAndAwningsRequestDTO: Codable, @unchecked Sendable {
     
     public let blindIds: [UUID]
+    public let controllerStateListIdsBlinds: [UUID]?
     public let awningIds: [UUID]
+    public let controllerStateListIdsAwnings: [UUID]?
     
-    public init(blindIds: [UUID], awningIds: [UUID]) {
+    public init(blindIds: [UUID],
+                controllerStateListIdsBlinds: [UUID]?,
+                awningIds: [UUID],
+                controllerStateListIdsAwnings: [UUID]?)
+    {
         self.blindIds = blindIds
+        self.controllerStateListIdsBlinds = controllerStateListIdsBlinds
         self.awningIds = awningIds
+        self.controllerStateListIdsAwnings = controllerStateListIdsAwnings
     }
 }
