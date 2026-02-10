dec6586f01fb68a2968fb0e8159662d890d5875b
VisualControl <system.visualcontrol@icloud.com>
2026-02-10 17:52:38 +0100

    Update BaosTimerValuesCollectionResponseDTO.swift

--- Sources/VisualControlSharedDTO/60 AutoChannelReq&RespDTO/63 BaosTimerValues/BaosTimerValuesCollectionResponseDTO.swift
+++ Sources/VisualControlSharedDTO/60 AutoChannelReq&RespDTO/63 BaosTimerValues/BaosTimerValuesCollectionResponseDTO.swift
@@ -17,32 +17,35 @@ public struct BaosTimerValuesCollectionResponseDTO: Codable, @unchecked Sendable
     public struct TimerValues: Codable, @unchecked Sendable {
         
         // GV.TimerXY._
-        public let baosDeviceValuesId: UUID
-        public let autoChannelNameBaos: String
+        public let baosDeviceValuesId: UUID?
+        public let baosDpName: String
         public let hourLeftSide: Int
         public let minutesLeftSide: Int
         public let mondayUntilFriday: Bool
         public let saturdayAndSunday: Bool
         public let hourRightSide: Int
         public let minutesRightSide: Int
+        public var currentStateTimer: Bool
         
-        public init(baosDeviceValuesId: UUID,
-             autoChannelNameBaos: String,
-             hourLeftSide: Int,
-             minutesLeftSide: Int,
-             mondayUntilFriday: Bool,
-             saturdayAndSunday: Bool,
-             hourRightSide: Int,
-             minutesRightSide: Int)
+        public init(baosDeviceValuesId: UUID?,
+                    baosDpName: String,
+                    hourLeftSide: Int,
+                    minutesLeftSide: Int,
+                    mondayUntilFriday: Bool,
+                    saturdayAndSunday: Bool,
+                    hourRightSide: Int,
+                    minutesRightSide: Int,
+                    currentStateTimer: Bool)
         {
             self.baosDeviceValuesId = baosDeviceValuesId
-            self.autoChannelNameBaos = autoChannelNameBaos
+            self.baosDpName = baosDpName
             self.hourLeftSide = hourLeftSide
             self.minutesLeftSide = minutesLeftSide
             self.mondayUntilFriday = mondayUntilFriday
             self.saturdayAndSunday = saturdayAndSunday
             self.hourRightSide = hourRightSide
             self.minutesRightSide = minutesRightSide
+            self.currentStateTimer = currentStateTimer
         }
     }
     
