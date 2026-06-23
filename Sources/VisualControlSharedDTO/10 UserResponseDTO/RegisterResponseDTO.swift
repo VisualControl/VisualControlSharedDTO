b4ddf9302653c31b5fd198e68a88eacba515ff8d
VisualControl <system.visualcontrol@icloud.com>
2026-06-23 09:13:31 +0200

    Update RegisterRespondeDTO

--- Sources/VisualControlSharedDTO/10 UserResponseDTO/RegisterResponseDTO.swift
+++ Sources/VisualControlSharedDTO/10 UserResponseDTO/RegisterResponseDTO.swift
@@ -10,11 +10,15 @@ import Foundation
 public struct RegisterResponseDTO: Codable, @unchecked Sendable {
     
     public let error: Bool
-    public var reason: String?
+    public let reason: String?
+    public let id: UUID?
+    public let username: String?
     
-    public init(error: Bool, reason: String? = nil) {
+    public init(error: Bool, reason: String? = nil, id: UUID? = nil, username: String? = nil) {
         self.error = error
         self.reason = reason
+        self.id = id
+        self.username = username
     }
     
 }
