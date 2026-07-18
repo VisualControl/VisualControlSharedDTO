679953cac686204823bb05aac670f3e531113608
VisualControl <system.visualcontrol@icloud.com>
2026-07-18 08:50:05 +0200

    Update VcChartPageDataRequestDTO.swift

--- Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/VcChartPageDataRequestDTO.swift
+++ Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/VcChartPageDataRequestDTO.swift
@@ -9,21 +9,18 @@ import Foundation
 
 public struct VcChartPageDataRequestDTO: Codable, @unchecked Sendable {
     
-    public let visualControlConfigId: UUID
     public let sequenceNumber: Int
     public let chartPageNameVisu: String
     public let chartPageNameSidebar: String
     public let creatingDate: Date
     public let authorizedUsers: [String]
     
-    public init(visualControlConfigId: UUID,
-                sequenceNumber: Int,
+    public init(sequenceNumber: Int,
                 chartPageNameVisu: String,
                 chartPageNameSidebar: String,
                 creatingDate: Date,
                 authorizedUsers: [String])
     {
-        self.visualControlConfigId = visualControlConfigId
         self.sequenceNumber = sequenceNumber
         self.chartPageNameVisu = chartPageNameVisu
         self.chartPageNameSidebar = chartPageNameSidebar
