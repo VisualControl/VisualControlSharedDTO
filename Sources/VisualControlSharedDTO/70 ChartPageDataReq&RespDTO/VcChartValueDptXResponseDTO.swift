2f9202b9507a945298431c2f594fbc3cb6b3d884
VisualControl <system.visualcontrol@icloud.com>
2026-07-19 15:27:49 +0200

    Update VcChartValueDptXResponseDTO.swift

--- Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/VcChartValueDptXResponseDTO.swift
+++ Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/VcChartValueDptXResponseDTO.swift
@@ -10,20 +10,6 @@ import Foundation
 public struct VcChartValueDpt01ResponseDTO: Codable, @unchecked Sendable {
     
     public let id: UUID
-    public let chartValueName: String
-    public let storedValuesDpt01: [VcStoredValueDpt01ResponseDTO]
-    
-    public init(id: UUID, chartValueName: String, storedValuesDpt01: [VcStoredValueDpt01ResponseDTO]) {
-        self.id = id
-        self.chartValueName = chartValueName
-        self.storedValuesDpt01 = storedValuesDpt01
-    }
-}
-
-
-public struct VcStoredValueDpt01ResponseDTO: Codable, @unchecked Sendable {
-    
-    public let id: UUID
     public let storedValue: Bool
     public let storageTime: Date
     
