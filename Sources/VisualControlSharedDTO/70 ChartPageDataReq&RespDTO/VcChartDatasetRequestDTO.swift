74f14a83b52dfcc59a37c6932b286cae78eed683
VisualControl <system.visualcontrol@icloud.com>
2026-07-20 11:42:46 +0200

    Update VcChartDatasetRequestDTO.swift

--- Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/VcChartDatasetRequestDTO.swift
+++ Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/VcChartDatasetRequestDTO.swift
@@ -12,17 +12,14 @@ public struct VcChartDatasetRequestDTO: Codable, @unchecked Sendable {
     public let chartNameVisu: String
     public let chartNameBaos: String
     public let chartValueDptType: String
-    public let creatingDate: Date
     
     public init(chartNameVisu: String,
                 chartNameBaos: String,
-                chartValueDptType: String,
-                creatingDate: Date)
+                chartValueDptType: String)
     {
         self.chartNameVisu = chartNameVisu
         self.chartNameBaos = chartNameBaos
         self.chartValueDptType = chartValueDptType
-        self.creatingDate = creatingDate
     }
     
 }
