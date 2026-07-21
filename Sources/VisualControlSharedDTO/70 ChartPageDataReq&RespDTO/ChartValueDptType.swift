a87e015b54cf915de66092c4b231ff057a4355b1
VisualControl <system.visualcontrol@icloud.com>
2026-07-21 11:16:37 +0200

    Update ChartValueDptType.swift

--- Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/ChartValueDptType.swift
+++ Sources/VisualControlSharedDTO/70 ChartPageDataReq&RespDTO/ChartValueDptType.swift
@@ -35,28 +35,30 @@ public enum ChartValueDptType: String, Codable, CaseIterable, Identifiable {
         }
     }
     
-    public static func convertToRawValue(_ array: [UInt8], type: ChartValueDptType) -> Any {
+    public static func convertToRawValue(_ byteArray: [Int], type: ChartValueDptType) -> Any {
         switch type {
             
         case .dpt01: // Bool
-            return array == [1] ? true : false
+            return byteArray == [1] ? true : false
             
         case .dpt05: // UInt8
-            let value = array.withUnsafeBytes { rawPtr -> UInt8 in
+            let bytes = generateByteArray(bytes: 1, from: byteArray)
+            let value = bytes.withUnsafeBytes { rawPtr -> UInt8 in
                 let loaded = rawPtr.load(as: UInt8.self)
                 return UInt8(bigEndian: loaded)
             }
             return Int(value)
             
         case .dpt06: // Int8
-            let value = array.withUnsafeBytes { rawPtr -> Int8 in
+            let bytes = generateByteArray(bytes: 1, from: byteArray)
+            let value = bytes.withUnsafeBytes { rawPtr -> Int8 in
                 let loaded = rawPtr.load(as: Int8.self)
                 return Int8(bigEndian: loaded)
             }
             return Int(value)
             
         case .dpt07: // UInt16
-            let bytes = generateByteArray(bytes: 2, from: array)
+            let bytes = generateByteArray(bytes: 2, from: byteArray)
             let value = bytes.withUnsafeBytes { rawPtr -> UInt16 in
                 let loaded = rawPtr.load(as: UInt16.self)
                 return UInt16(bigEndian: loaded)
@@ -64,7 +66,7 @@ public enum ChartValueDptType: String, Codable, CaseIterable, Identifiable {
             return Int(value)
             
         case .dpt08: // Int16
-            let bytes = generateByteArray(bytes: 2, from: array)
+            let bytes = generateByteArray(bytes: 2, from: byteArray)
             let value = bytes.withUnsafeBytes { rawPtr -> Int16 in
                 let loaded = rawPtr.load(as: Int16.self)
                 return Int16(bigEndian: loaded)
@@ -72,7 +74,7 @@ public enum ChartValueDptType: String, Codable, CaseIterable, Identifiable {
             return Int(value)
             
         case .dpt09: // Float16
-            let bytes = generateByteArray(bytes: 2, from: array)
+            let bytes = generateByteArray(bytes: 2, from: byteArray)
             let bits = bytes.withUnsafeBytes { rawPtr -> UInt16 in
                 let loaded = rawPtr.load(as: UInt16.self)
                 return UInt16(bigEndian: loaded)
@@ -84,7 +86,7 @@ public enum ChartValueDptType: String, Codable, CaseIterable, Identifiable {
             return Float(float)
             
         case .dpt12: // UInt32
-            let bytes = generateByteArray(bytes: 4, from: array)
+            let bytes = generateByteArray(bytes: 4, from: byteArray)
             let value = bytes.withUnsafeBytes { rawPtr -> UInt32 in
                 let loaded = rawPtr.load(as: UInt32.self)
                 return UInt32(bigEndian: loaded)
@@ -92,7 +94,7 @@ public enum ChartValueDptType: String, Codable, CaseIterable, Identifiable {
             return Int(value)
             
         case .dpt13: // Int32
-            let bytes = generateByteArray(bytes: 4, from: array)
+            let bytes = generateByteArray(bytes: 4, from: byteArray)
             let value = bytes.withUnsafeBytes { rawPtr -> Int32 in
                 let loaded = rawPtr.load(as: Int32.self)
                 return Int32(bigEndian: loaded)
@@ -100,7 +102,7 @@ public enum ChartValueDptType: String, Codable, CaseIterable, Identifiable {
             return Int(value)
             
         case .dpt14: // Float32
-            let bytes = generateByteArray(bytes: 4, from: array)
+            let bytes = generateByteArray(bytes: 4, from: byteArray)
             let bits = bytes.withUnsafeBytes { rawPtr -> UInt32 in
                 let loaded = rawPtr.load(as: UInt32.self)
                 return UInt32(bigEndian: loaded)
@@ -112,8 +114,8 @@ public enum ChartValueDptType: String, Codable, CaseIterable, Identifiable {
     }
 }
 
-func generateByteArray(bytes: Int, from byteArray: [UInt8]) -> [UInt8] {
-    var modifiedByteArray: [UInt8] = byteArray
+func generateByteArray(bytes: Int, from byteArray: [Int]) -> [UInt8] {
+    var modifiedByteArray: [UInt8] = byteArray.map { UInt8($0) }
     if modifiedByteArray.count < bytes {
         while modifiedByteArray.count != bytes {
             modifiedByteArray.insert(0, at: 0)
