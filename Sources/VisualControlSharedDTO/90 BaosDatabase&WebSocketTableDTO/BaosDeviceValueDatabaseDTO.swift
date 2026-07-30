0badbdebc6ae35f8c936bb0822fcf3d0299b23eb
VisualControl <system.visualcontrol@icloud.com>
2026-07-30 23:44:00 +0200

    Update BaosDeviceValueDatabaseDTO.swift

--- Sources/VisualControlSharedDTO/90 BaosDatabase&WebSocketTableDTO/BaosDeviceValueDatabaseDTO.swift
+++ Sources/VisualControlSharedDTO/90 BaosDatabase&WebSocketTableDTO/BaosDeviceValueDatabaseDTO.swift
@@ -14,7 +14,7 @@ public struct BaosDeviceValueDatabaseDTO: Codable, @unchecked Sendable {
     public let baosDpId: Int
     public let baosDpName: String
     public let zoneNameBaos: String
-    public let dptType: String
+    public let dptType: Int
     public let dpRawValue: [Int]
     public let communicationFlag: Bool
     public let readFlag: Bool
@@ -30,7 +30,7 @@ public struct BaosDeviceValueDatabaseDTO: Codable, @unchecked Sendable {
                 baosDpId: Int,
                 baosDpName: String,
                 zoneNameBaos: String,
-                dptType: String,
+                dptType: Int,
                 dpRawValue: [Int],
                 communicationFlag: Bool,
                 readFlag: Bool,
